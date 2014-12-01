325fa870726d911945bba08f5b41c4f9d3fef224
asus <asus@asus-PC>
2014-12-01 11:44:43 +0800

    Command unused class and test

--- src/tugas05_client/Tugas05_client.java
+++ src/tugas05_client/Tugas05_client.java
@@ -182,7 +182,7 @@ public class Tugas05_client {
     }*/
     
     private static void readKey() throws IOException, InterruptedException {
-        int buf, using = 0;
+        int buf;
         byte[] mybytearray = new byte[16384]; //16384
         while (System.in.available() > 0) {
             buf = System.in.read();
